200b5d71e91f94dc51a8ab26cbe168c454c201ef
Christopher Kennedy <cjkb90@users.noreply.github.com>
2016-01-10 18:46:16 -0500

    Finished project without sending emails

--- tumblr_mailer.js
+++ tumblr_mailer.js
@@ -1,8 +1,15 @@
 var fs = require('fs');
-var ejs = require('ejs');
 var csvFile = fs.readFileSync("friend_list.csv","utf8");
 var emailTemplate = fs.readFileSync("email_template.html", "utf-8")
-var customizedTemplate = ejs.render(emailTemplate);
+var emailTemplateEJS = fs.readFileSync("email_template.ejs","utf-8")
+var ejs = require('ejs');
+var tumblr = require('tumblr.js')
+var client = tumblr.createClient({
+  consumer_key: 'xx',
+  consumer_secret: 'xx',
+  token: 'xx',
+  token_secret: 'xx'
+});
 
 var csvParse = function(file){
   //Set arr to an array where each row is an element, starting with the header
@@ -27,11 +34,33 @@ var csvParse = function(file){
 
 var contactData = csvParse(csvFile);
 
-//write a function that will console.log a personalized email for each person
-//function will grab the string value of the email template, and for each object in contactData, replace the placeholders with real values
 var customEmail = function(data, html){
   for (i=0;i<data.length;i++){
     var htmlCustom = html.replace("FIRST_NAME", data[i].firstName).replace("NUM_MONTHS_SINCE_CONTACT",data[i].numMonthsSinceContact);
     console.log(htmlCustom);
   }
 }
+
+
+client.posts('cjkb90.tumblr.com', function(err, blog){
+  var latestPosts = [];
+  for (i=0;i<blog.posts.length;i++){
+      var blogDate = new Date(blog.posts[i].date.slice(0,11));
+      var oneWeekAgo = new Date(Date.now());
+      oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+      if (blogDate > oneWeekAgo){
+        latestPosts.push(blog.posts[i]);
+      }
+  }
+  var makeTemplate = function(data){
+  for(i=0;i<data.length;i++){
+    var customizedTemplate = ejs.render(emailTemplateEJS, 
+                    { firstName: data[i].firstName,  
+                      numMonthsSinceContact: data[i].numMonthsSinceContact,
+                      latestPosts: latestPosts
+                    });
+    console.log(customizedTemplate);
+  }
+}
+  makeTemplate(contactData);
+})
